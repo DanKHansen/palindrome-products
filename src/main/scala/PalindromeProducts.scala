b8bae300bac5cc1a07afb6c8e546304a5ad85c3e
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-13 17:21:50 +0200

    in process

--- src/main/scala/PalindromeProducts.scala
+++ src/main/scala/PalindromeProducts.scala
@@ -1,3 +1,14 @@
-case class PalindromeProducts(i:Int, j:Int):
-   val smallest: Option[(Int, Set[(Int, Int)])] = ???
-   val largest: Option[(Int, Set[(Int, Int)])] = ???
+case class PalindromeProducts(i: Int, j: Int):
+   lazy val l: Seq[(Int, Set[(Int, Int)])] = for
+      a <- i to j
+      b <- a to j
+      if (b * a).toString == (b * a).toString.reverse
+   yield (a * b, Set((a, b)))
+
+   val smallest: Option[(Int, Set[(Int, Int)])] =
+      if l.nonEmpty then Some(l.minBy(_._1)) else None
+
+   val largest: Option[(Int, Set[(Int, Int)])] = {
+      println(l.groupBy(_._1).values)
+      if l.nonEmpty then Some(l.maxBy(_._1)) else None
+   }
