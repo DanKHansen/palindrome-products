6313930a915d4560335fa1f94b1eeaed84d1e226
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-14 09:32:52 +0200

    refactor

--- src/main/scala/PalindromeProducts.scala
+++ src/main/scala/PalindromeProducts.scala
@@ -8,6 +8,5 @@ case class PalindromeProducts(i: Int, j: Int):
    val smallest: Option[(Int, Set[(Int, Int)])] =
       if l.nonEmpty then Some(l.minBy(_._1)) else None
 
-   val largest: Option[(Int, Set[(Int, Int)])] = {
+   val largest: Option[(Int, Set[(Int, Int)])] =
       if l.nonEmpty then Some((l.maxBy(_._1)._1, l.distinct.groupBy(_._1).maxBy(_._1)._2.flatMap(_._2).toSet)) else None
-   }
